557e4e39f088cc220d895fab942d3a2936992f11
Андрей <barcats@inbox.ru>
2019-05-23 13:06:20 +0300

    никогда не делать Merge не понимая

---
+++ app/src/main/java/ru/bartex/smetaelectro/BehaviorWorkOrMat_Mat.java
@@ -0,0 +1,10 @@
+package ru.bartex.smetaelectro;
+
+import android.content.Context;
+
+public class BehaviorWorkOrMat_Mat implements BehaviorWorkOrMat {
+    @Override
+    public void updateAdapter(Context context) {
+
+    }
+}
---
+++ app/src/main/java/ru/bartex/smetaelectro/BehaviorWorkOrMat_Work.java
@@ -0,0 +1,10 @@
+package ru.bartex.smetaelectro;
+
+import android.content.Context;
+
+public class BehaviorWorkOrMat_Work implements BehaviorWorkOrMat {
+    @Override
+    public void updateAdapter(Context context) {
+
+    }
+}
